196cd1a807e21c60c1588def184d0fc2b7e75afe
gleamicus <gh@debacle.org>
2014-08-12 22:23:18 -0500

    Switch to current apt-get-update resource
    [This commit](https://github.com/opscode-cookbooks/apt/commit/d73d0fc9ac989940c9905da5be75dedc84c83bcb) renamed the resource from 'apt-get update' to 'apt-get-update'.

--- recipes/default.rb
+++ recipes/default.rb
@@ -27,7 +27,7 @@ template '/etc/apt/sources.list' do
     :include_source_packages => node['ubuntu']['include_source_packages'],
     :components => node['ubuntu']['components']
     )
-  notifies :run, 'execute[apt-get update]', :immediately
+  notifies :run, 'execute[apt-get-update]', :immediately
   source 'sources.list.erb'
 end
 
